daba1c5445c5430e2c89ccba2f1c4392e712146e
bofa26 <woyenghilabofa@outlook.com>
2023-07-08 00:08:06 +0100

    Create logger.php

---
+++ Pandascrow/Logger/logger.php
@@ -0,0 +1,63 @@
+<?php  
+
+/**
+ * 
+ */
+class Logger
+{
+	/**
+	 * 
+	 * @var string
+	 * 
+	 * 
+	 */
+	public $handler = "";
+	/**
+	 * 
+	 * @var string
+	 * 
+	 * 
+	 */
+	public $content = "";
+	
+	function __construct(string $log_path = "")
+	{
+		if ($log_path == "") {
+			$defaultpath = dirname(dirname(__DIR__))."/Log";
+			if (! is_dir($defaultpath)) {
+				mkdir($defaultpath);
+			}
+			$path = $defaultpath."/".date("Y-m-d").'.log';
+			$this->handler = fopen($path, 'a');
+		}else{
+			if (! is_dir($log_path)) {
+				mkdir($log_path, 0777, true);
+			}
+			$path = $log_path."/".date("Y-m-d").".log";
+			$this->handler = fopen($path, 'a');
+		}
+	}
+
+	public function format(string $level, string $date, string $message)
+	{
+		$level = strtoupper($level);
+		$message = ucfirst($message);
+		$fmt = sprintf("\n %s|[%s]|%s", $level, $date, $message);
+		$this->content = $fmt;
+	}
+
+	public function pen()
+	{
+		if (! $this->handler) {
+			throw new \Exception("Log file missing");
+		}
+		fwrite($this->handler, $this->content);
+	}
+
+	public function log(string $level, string $message)
+	{
+		$date = date("Y-m-d H:i:s");
+		$this->format($level, $date, $message);
+		$this->pen();
+	}
+}
